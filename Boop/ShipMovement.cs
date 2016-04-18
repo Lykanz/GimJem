74553ce9c0d221485709460f8d64591d9c4106dd
ShortcutSam <sengsamkiat@gmail.com>
2016-04-18 20:05:42 +0800

    Basic Ship Movement Tested and Ready

--- Boop/ShipMovement.cs
+++ Boop/ShipMovement.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShipMovement : MonoBehaviour {
+public class ShipMovement : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    [SerializeField] private float max_VerticalSpeed = 0.5f;
+    [SerializeField] private float max_HorizontalSpeed = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey("w"))
+        {
+            m_Rigidbody.AddRelativeForce(Vector3.up * max_VerticalSpeed);
+        }
+        else if (Input.GetKey("a"))
+        {
+            m_Rigidbody.AddRelativeForce(-Vector3.right * max_HorizontalSpeed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            m_Rigidbody.AddRelativeForce(-Vector3.up * max_HorizontalSpeed * max_VerticalSpeed);
+        }
+        else if (Input.GetKey("d"))
+        {
+            m_Rigidbody.AddRelativeForce(Vector3.right * max_HorizontalSpeed);
+        }
 	}
 }
