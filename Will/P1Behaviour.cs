4a3102f26c5574140ffb1cdb8a390f32a15b2116
wilakuma <mebewilliam@gmail.com>
2016-04-19 13:51:42 +0800

    Portals

---
+++ Will/P1Behaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P1Behaviour : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player") {
+		//Display the button to press
+		}
+	}
+}
